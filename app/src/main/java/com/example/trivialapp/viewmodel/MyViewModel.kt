c30e08a72b39fac097610a02ae8aba2d6c9ded61
martiespinosa1 <marti.espinosa.7e7@itb.cat>
2024-01-17 10:09:43 +0100

    mas

--- app/src/main/java/com/example/trivialapp/viewmodel/MyViewModel.kt
+++ app/src/main/java/com/example/trivialapp/viewmodel/MyViewModel.kt
@@ -3,11 +3,14 @@ package com.example.trivialapp.viewmodel
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 
 class MyViewModel: ViewModel() {
+    var selectedDifficulty: String by mutableStateOf("Easy")
+        private set
 
-
-
-
+    fun modifyDifficulty(difficulty: String) {
+        selectedDifficulty = difficulty
+    }
 }
