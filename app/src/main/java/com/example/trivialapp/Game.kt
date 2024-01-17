92b68d9bb4d21f5dedf48479e2ecc3abb8768772
martiespinosa1 <marti.espinosa.7e7@itb.cat>
2024-01-17 09:08:14 +0100

    game screen

--- app/src/main/java/com/example/trivialapp/Game.kt
+++ app/src/main/java/com/example/trivialapp/Game.kt
@@ -1,5 +1,6 @@
 package com.example.trivialapp
 
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -8,6 +9,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.layout.requiredWidth
@@ -27,7 +29,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.VectorProperty
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
@@ -42,10 +46,8 @@ import java.util.Random
 
 @Composable
 fun Game(navController: NavController) {
-
-    var randomWord by remember { mutableStateOf("") }
-    val random = Random()
-    var attempts by remember { mutableIntStateOf(0) }
+    var round by remember { mutableIntStateOf(1) }
+    var totalRounds by remember { mutableIntStateOf(10) }
 
     Image(
         painter = painterResource(id = R.drawable.fondo),
@@ -56,16 +58,116 @@ fun Game(navController: NavController) {
 
     Column(
         modifier = Modifier
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
+            .fillMaxSize()
+            .padding(25.dp),
+        verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
         Text(
-            text = "Game screen", fontFamily = FontFamily.Monospace, fontWeight = FontWeight.Bold,
+            text = "Round $round/$totalRounds", fontFamily = FontFamily.Monospace, fontWeight = FontWeight.Bold,
             fontSize = 24.sp,
             modifier = Modifier.padding(top = 15.dp)
         )
 
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 100.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Question here",
+                fontFamily = FontFamily.Monospace,
+                fontWeight = FontWeight.Bold,
+                fontSize = 40.sp,
+                modifier = Modifier.padding(top = 15.dp)
+            )
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 80.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            OutlinedButton(
+                onClick = { navController.navigate(Routes.Menu.route) },
+                modifier = Modifier.requiredWidth(150.dp),
+            ) {
+                Text(text = "Answer 1", fontSize = 20.sp, fontFamily = FontFamily.Monospace, fontWeight = FontWeight.Bold)
+            }
+            Spacer(modifier = Modifier.padding(10.dp))
+            OutlinedButton(
+                onClick = { navController.navigate(Routes.Menu.route) },
+                modifier = Modifier.requiredWidth(150.dp),
+            ) {
+                Text(text = "Answer 2", fontSize = 20.sp, fontFamily = FontFamily.Monospace, fontWeight = FontWeight.Bold)
+            }
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 50.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            OutlinedButton(
+                onClick = { navController.navigate(Routes.Menu.route) },
+                modifier = Modifier.requiredWidth(150.dp),
+            ) {
+                Text(text = "Answer 3", fontSize = 20.sp, fontFamily = FontFamily.Monospace, fontWeight = FontWeight.Bold)
+            }
+            Spacer(modifier = Modifier.padding(10.dp))
+            OutlinedButton(
+                onClick = { navController.navigate(Routes.Menu.route) },
+                modifier = Modifier.requiredWidth(150.dp),
+            ) {
+                Text(text = "Answer 4", fontSize = 20.sp, fontFamily = FontFamily.Monospace, fontWeight = FontWeight.Bold)
+            }
+        }
+
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 100.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            TimeBar(30, 15)
+        }
+
     }
 }
+
+
+@Composable
+fun TimeBar(totalDuration: Int, currentProgress: Int) {
+    val reversedProgress = totalDuration - currentProgress // Invierte el progreso actual
+
+    Canvas(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(30.dp)
+    ) {
+        // Dibuja la barra de fondo
+        drawLine(
+            color = Color.LightGray,
+            start = Offset(0f, size.height / 2),
+            end = Offset(size.width, size.height / 2),
+            strokeWidth = 30f
+        )
+
+        // Dibuja la barra de progreso invertida
+        drawLine(
+            color = Color.Blue,
+            start = Offset(size.width * (reversedProgress.toFloat() / totalDuration), size.height / 2),
+            end = Offset(size.width, size.height / 2),
+            strokeWidth = 30f
+        )
+    }
+}
+
