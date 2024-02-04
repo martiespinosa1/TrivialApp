37780dd92f3b695261c3dd4d4fba7b4642df3c77
marti <martiespinosa99@gmail.com>
2024-02-04 14:55:41 +0100

    100 preguntas de cada dificultad

--- app/src/main/java/com/example/trivialapp/model/QuestionsAndAnswers.kt
+++ app/src/main/java/com/example/trivialapp/model/QuestionsAndAnswers.kt
@@ -3,6 +3,7 @@ package com.example.trivialapp.model
 class QuestionsAndAnswers {
     data class Kahoot(
         val pregunta: String,
-        var respuestas: MutableList<String>
+        var respuestas: MutableList<String>,
+        val tipo: String
     )
 }
